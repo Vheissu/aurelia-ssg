934af7fdbfd2970778f8904b13db00a3e871badb
Dwayne Charrington <dwaynecharrington@gmail.com>
2023-03-04 20:37:25 +1000

    feat(ssg): functional but flawed

--- src/ssg.ts
+++ src/ssg.ts
@@ -1,7 +1,8 @@
 import express, { NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import puppeteer from 'puppeteer';
-import { resolve } from 'path';
+import path from 'path';
+import fs from 'fs';
 
 function init() {
     const app = express();
@@ -20,29 +21,56 @@ function init() {
 }
 
 async function processRenderRequest(request: Request, response: Response) {
-    let url = request.originalUrl.split('/render?url=')[1];
-
-    const page = await render(url);
-
-    console.log(`Rendering: ${page}`);
-
-    return response.status(200).send(page);
+    const urlParam = request.query.url as string;
+  
+    if (!urlParam) {
+      return response.sendStatus(400);
+    }
+  
+    const url = `http://localhost:9000${urlParam}`;
+  
+    const html = await render(url);
+  
+    // Generate static file
+    const filePath = path.join(__dirname, 'public', `${urlParam.replace(/\//g, '_')}.html`);
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    fs.writeFileSync(filePath, html);
+  
+    console.log(`Rendering: ${url}`);
+  
+    return response.status(200).send(html);
 }
 
 async function render(url: string): Promise<string> {
     const browser = await puppeteer.launch({
-        args: [
-            '--enable-features=ExperimentalJavaScript'
-        ]
+      args: [
+        '--enable-features=ExperimentalJavaScript'
+      ]
     });
+  
     const page = await browser.newPage();
-
-    await page.goto(url, { timeout: 15000 });
-    const html = await page.content();
-
-    page.close();
-
-    return html;
-}
+    try {
+      await page.goto(url, { timeout: 15000 });
+  
+      // Wait for the my-app element to have child elements
+      await page.waitForFunction(() => {
+        const myApp = document.querySelector('my-app');
+        return myApp && myApp.childElementCount > 0;
+      }, { timeout: 5000 });
+  
+      const html = await page.content();
+  
+      await page.close();
+      await browser.close();
+  
+      return html;
+    } catch (err) {
+      console.error(`Error rendering ${url}: ${err}`);
+      await page.close();
+      await browser.close();
+      throw err;
+    }
+  }
+  
 
 init();
